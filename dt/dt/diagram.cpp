e99561a50af8a0cacea7ec55319497598f90fe9a
sylph520 <pallasjack@outlook.com>
2016-07-21 21:09:56 +0800

    second mod

--- dt/dt/diagram.cpp
+++ dt/dt/diagram.cpp
@@ -1,6 +1,6 @@
 # include "stdafx.h"
 # include "diagram.h"
-#include "zhangsuen.h"
+
 Mat image_binarizing(Mat input_image)
 {
 	/* This function is used for transform image to its binarized image */
@@ -161,7 +161,11 @@ void detect_circle(Mat diagram_segment, Mat &color_img,Mat &diagram_segwithoutci
 			}
 			++nIter;
 		}
-		if (bestCVal > 300)
+		if (bestCVal<4 * bestCircleRadius) 
+			break;
+		if (bestCVal<(int)(edgePositions.size() / 8)) 
+			break;
+		if (bestCVal > 200)
 		{
 			//std::cout << "current best circle: " << bestCircleCenter << " with radius: " << bestCircleRadius << " and nInlier " << bestCVal << endl;
 			Vec3f circle = { bestCircleCenter.x, bestCircleCenter.y, bestCircleRadius };
@@ -689,16 +693,26 @@ void detect_line3(Mat diagram_segwithoutcircle, Mat &color_img, vector<Vec4i> &l
 {
 	vector<Vec4i> rawLines;
 	HoughLinesP(diagram_segwithoutcircle, rawLines, 1, CV_PI / 180, 15, 15, 10);
+
 	for (size_t i = 0; i < rawLines.size(); ++i)
 	{
+		//eliminate the false detected lines with few points on it
 		Vec4i rawLine = rawLines[i];
 		float lineEV = evaluateLine(diagram_segwithoutcircle, rawLine);
-		if (lineEV > 15)
+		if (lineEV > 30)
 		{
 			lines.push_back(rawLine);
-			line(diagram_segwithoutcircle, Point(rawLine[0], rawLine[1]), Point(rawLine[2], rawLine[3]), Scalar(0, 0, 0), 6, 8);
+			line(diagram_segwithoutcircle, Point(rawLine[0], rawLine[1]), Point(rawLine[2], rawLine[3]), Scalar(0, 0, 0), 2, 8);
 		}
 	}
+	for (size_t j = 0; j < lines.size(); ++j)
+	{
+		Vec4i l = lines[j];
+		line(color_img, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 255, 0), 2, 8);
+	}
+	namedWindow("lines first opt version now"); imshow("lines first opt version now", color_img);
+
+
 }
 void primitive_parse(Mat diagram_segment, vector<pointX> &points, vector<lineX> &lines, vector<circleX> &circles)
 {
@@ -739,7 +753,7 @@ void primitive_parse(Mat diagram_segment, vector<pointX> &points, vector<lineX> 
 int test_diagram()
 {
 	//first load a image
-	Mat image = imread("Sg-2.jpg", 0);
+	Mat image = imread("Sg-1.jpg", 0);
 	//namedWindow("original image");
 	//imshow("original image", image);
 	// then binarize it
